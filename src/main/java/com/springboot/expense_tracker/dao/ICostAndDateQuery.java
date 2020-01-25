704f35df6b57ada32ca21e803eda973c467c7b6c
Ujjaval Desai <ujjaval@Ujjavals-MacBook-Pro.local>
2020-01-24 23:28:26 -0600

    initial commit

---
+++ src/main/java/com/springboot/expense_tracker/dao/ICostAndDateQuery.java
@@ -0,0 +1,6 @@
+package com.springboot.expense_tracker.dao;
+
+public interface ICostAndDateQuery {
+	Integer getCost();
+	String getDate();
+}
---
+++ src/main/java/com/springboot/expense_tracker/dao/ICostAndMonthQuery.java
@@ -0,0 +1,6 @@
+package com.springboot.expense_tracker.dao;
+
+public interface ICostAndMonthQuery {
+	public Integer getCost();
+	public String getMonth();
+}
